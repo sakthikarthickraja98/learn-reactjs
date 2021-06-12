888dc7d4ca3d78af1a494694d9b895530a69f63a
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-12 10:28:30 +0530

    first commit

---
+++ learn-react/src/app.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+
+export default class App extends Component{
+    render(){
+        return (
+           <React.Fragment>
+            <h4>Hello!</h4>
+           </React.Fragment>
+        );
+    }
+}
