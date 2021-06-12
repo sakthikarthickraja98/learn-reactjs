b21f0f14e6d26d5dc098567c398db4d0b6d54bba
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-12 17:10:43 +0530

    create approval card

--- learn-react/src/app.jsx
+++ learn-react/src/app.jsx
@@ -1,11 +1,13 @@
 import fake from "faker";
 import React, { Component } from "react";
 import Commenddetails from "./commenddetails";
+import Approvalcard from "./approvalcard";
 
 export default class App extends Component {
   render() {
     return (
       <div className="ui container comments">
+          <Approvalcard/>
         <Commenddetails author="Kadir" timeago="Today at 5:00PM" content="Lovable content" image={fake.image.avatar()}/>
         <Commenddetails author="Murugan" timeago="Today at 4:30PM" content="I like this writing" image={fake.image.avatar()}/>
         <Commenddetails author="kandha" timeago="yesterday at 5:00PM" content="I like this subject" image={fake.image.avatar()}/>
